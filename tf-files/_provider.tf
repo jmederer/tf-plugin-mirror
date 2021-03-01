ea78562ada46a3b56411f0f2569fe54432f02d44
Jan Mederer <jan@adorsys.com>
2021-03-01 18:23:17 +0100

    uses checkout

---
+++ tf-files/_provider.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    kubernetes-alpha = {
+      source  = "hashicorp/kubernetes-alpha"
+      version = "0.2.1"
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = "2.0.2"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.0.1"
+    }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.7.0"
+    }
+  }
+}
