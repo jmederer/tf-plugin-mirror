d0bf06c751832642fd2d8ce94ec0688f258b166d
Svetoslav Batchovski <svetoslav@batchovski.de>
2021-04-16 10:04:59 +0200

    min versions + vault provider

--- tf-files/_provider.tf
+++ tf-files/_provider.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     helm = {
       source  = "hashicorp/helm"
-      version = "2.0.2"
+      version = ">= 2.0.2"
     }
     random = {
       source  = "hashicorp/random"
-      version = "3.0.1"
+      version = ">= 3.0.1"
     }
     kubectl = {
       source  = "gavinbunney/kubectl"
@@ -14,19 +14,23 @@ terraform {
     }
     tls = {
       source = "hashicorp/tls"
-      version = "3.1.0"
+      version = ">= 3.1.0"
     }
     rancher2 = {
       source = "rancher/rancher2"
-      version = "1.12.0"
+      version = ">= 1.12.0"
     }
     kafka = {
       source = "Mongey/kafka"
-      version = "0.3.0"
+      version = ">= 0.3.0"
     }
     kubernetes = {
       source = "hashicorp/kubernetes"
-      version = "2.1.0"
+      version = ">= 2.1.0"
+    }
+    vault = {
+      source = "hashicorp/vault"
+      version = ">= 2.19.0"
     }
   }
 }
