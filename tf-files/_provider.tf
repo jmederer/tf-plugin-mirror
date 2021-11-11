03f301357d121b6d251ca76449ffb922fbb6d435
Jan Mederer <jan@mederer.it>
2021-11-11 10:42:28 +0100

    Update minimum Versions

--- tf-files/_provider.tf
+++ tf-files/_provider.tf
@@ -2,15 +2,15 @@ terraform {
   required_providers {
     helm = {
       source  = "hashicorp/helm"
-      version = ">= 2.0.2"
+      version = ">= 2.4.1"
     }
     random = {
       source  = "hashicorp/random"
-      version = ">= 3.0.1"
+      version = ">= 3.1.0"
     }
     kubectl = {
       source  = "gavinbunney/kubectl"
-      version = ">= 1.7.0"
+      version = ">= 1.13.1"
     }
     tls = {
       source = "hashicorp/tls"
@@ -18,27 +18,27 @@ terraform {
     }
     rancher2 = {
       source = "rancher/rancher2"
-      version = ">= 1.12.0"
+      version = ">= 1.21.0"
     }
     kafka = {
       source = "Mongey/kafka"
-      version = ">= 0.4.0"
+      version = ">= 0.4.1"
     }
     kubernetes = {
       source = "hashicorp/kubernetes"
-      version = ">= 2.1.0"
+      version = ">= 2.6.1"
     }
     vault = {
       source = "hashicorp/vault"
-      version = ">= 2.19.0"
+      version = ">= 2.24.1"
     }
     aws = {
       source = "hashicorp/aws"
-      version = ">= 3.36.0"
+      version = ">= 3.64.2"
     }
     time = {
       source = "hashicorp/time"
-      version = ">=0.7.0"
+      version = ">=0.7.2"
     }
   }
 }
