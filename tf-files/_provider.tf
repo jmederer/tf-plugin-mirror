093855d6a4135d59837ce14beb09c5318316f6b2
Svetoslav Batchovski <svetoslav@batchovski.de>
2022-03-01 10:50:29 +0100

    fix(providers) Versions pinned

--- tf-files/_provider.tf
+++ tf-files/_provider.tf
@@ -2,43 +2,43 @@ terraform {
   required_providers {
     helm = {
       source  = "hashicorp/helm"
-      version = ">= 2.4.1"
+      version = "2.4.1"
     }
     random = {
       source  = "hashicorp/random"
-      version = ">= 3.1.0"
+      version = "3.1.0"
     }
     kubectl = {
       source  = "gavinbunney/kubectl"
-      version = ">= 1.13.1"
+      version = "1.13.1"
     }
     tls = {
       source = "hashicorp/tls"
-      version = ">= 3.1.0"
+      version = "3.1.0"
     }
     rancher2 = {
       source = "rancher/rancher2"
-      version = ">= 1.21.0"
+      version = "1.21.0"
     }
     kafka = {
       source = "Mongey/kafka"
-      version = ">= 0.4.1"
+      version = "0.4.1"
     }
     kubernetes = {
       source = "hashicorp/kubernetes"
-      version = ">= 2.6.1"
+      version = "2.6.1"
     }
     vault = {
       source = "hashicorp/vault"
-      version = ">= 2.24.1"
+      version = "2.24.1"
     }
     aws = {
       source = "hashicorp/aws"
-      version = ">= 3.64.2"
+      version = "3.64.2"
     }
     time = {
       source = "hashicorp/time"
-      version = ">=0.7.2"
+      version = "0.7.2"
     }
   }
 }
